4b204a00004e12ee660ee7c822abc7b6f7e27518
Bartosz Rodziewicz <bartoszka1996@gmail.com>
2019-04-22 23:26:12 +0200

    Add comments

--- Windows/USBTethering/tether-on.bat
+++ Windows/USBTethering/tether-on.bat
@@ -1,1 +1,3 @@
+REM https://stackoverflow.com/questions/20226924/is-it-possible-to-usb-tether-an-android-device-using-adb-through-the-terminal
+echo Turning on USB tethering - make sure the device is connected, the computer have adb permission granted and root access for adb is permitted.
 adb shell su -c service call connectivity 33 i32 1 s16 text
