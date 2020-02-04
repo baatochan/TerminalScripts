7197bf21410303151c119999c8429eed7f9a91ad
Bartosz Rodziewicz <bartoszka1996@gmail.com>
2020-02-04 17:49:45 +0100

    Add script for finding missing days

--- Linux/1SEFileManagment/export1SEFootage.sh
+++ Linux/1SEFileManagment/export1SEFootage.sh
@@ -3,7 +3,7 @@
 mkdir -p clips
 
 for f in *; do
-    if [[ -d "$f" ]]; then
+	if [[ -d "$f" ]]; then
 		if [[ -e "${f}/clip.mp4" ]]; then
 			#echo "$f"
 			mv "${f}/clip.mp4" "clips/${f}.mp4"
@@ -11,5 +11,5 @@ for f in *; do
 			#echo "$f"
 			echo "$f" >> missing.txt
 		fi
-    fi
+	fi
 done
