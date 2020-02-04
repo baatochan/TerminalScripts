dff08e0da68eddf768441d81a151371ec7b80c37
Bartosz Rodziewicz <bartoszka1996@gmail.com>
2020-02-04 17:50:56 +0100

    Fix end of month situation in pulling script

--- Linux/1SEFileManagment/pull.bat
+++ Linux/1SEFileManagment/pull.bat
@@ -3,7 +3,7 @@
 rmdir result /s /q
 mkdir result
 
-set numberOfDaysToPull=14
+set numberOfDaysToPull=16
 
 set day=%date:~0,2%
 set month=%date:~3,2%
@@ -15,9 +15,15 @@ set /A filename=%filename%-%numberOfDaysToPull%
 
 set /A terminator=%filename%+%numberOfDaysToPull%+1
 
+if %filename:~6,8%==00 set /A filename=%filename%-69
+
+if %filename:~6,8% GTR 31 set /A filename=%filename%-69
+
 :loop
+if %filename:~6,8% GTR 31 set /A filename=%filename%+69
+
 echo Pulling %filename%
-adb pull "/sdcard/.1second/time_cc_My Life/%filename%" result/
+adb pull -a "/sdcard/.1second/time_cc_My Life/%filename%" result/
 
 set /A filename=%filename%+1
 if %filename%==%terminator% goto endOfLoop
